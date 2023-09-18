9a809ca253e875834c2d8a92119df0e30a0fd4b9
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2023-09-18 17:18:39 +0200

    fixes

--- scripts/componentLoader.js
+++ scripts/componentLoader.js
@@ -51,11 +51,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     });
-
-  // Load the footer
-  fetch("../components/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("footer").innerHTML = data;
-    });
 });
